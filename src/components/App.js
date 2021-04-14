8ed03d236e462f05c6410f48eccf6dd44fafca64
Ashah15 <ashaabdillahi53@gmail.com>
2021-04-15 01:13:20 +0300

    Wrapped the app commponent in a fragment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
 export default function App() {
   return (
-    <div>
+    <>
       <Display />
       <ButtonPanel />
-    </div>
+    </>
   );
 }
