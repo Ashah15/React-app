e715cf36fe7d60ac2da07ba60b39ce37ee66115e
Ashah15 <ashaabdillahi53@gmail.com>
2021-04-23 20:43:49 +0300

    Removed the commented code

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -65,29 +65,3 @@ const calculate = (dataObj, buttonName) => {
 };
 
 export default calculate;
-
-// import operate from './operate';
-
-// const calculate = (data, name) => {
-//   let result;
-//   const { total, next, operation } = data;
-//   switch (name) {
-//     case '+/-':
-//       result = total * (-1);
-//       break;
-//     case '.':
-//       result = '.';
-//       break;
-//     case 'AC':
-//       result = '';
-//       break;
-//     case '=':
-//       result = operate(total, next, operation);
-//       break;
-//     default:
-//   }
-
-//   return result;
-// };
-
-// export default calculate;
