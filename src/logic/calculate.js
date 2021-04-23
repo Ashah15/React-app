d28fb1e7b23044094a08941605907eccf79c3cdb
Ashah15 <ashaabdillahi53@gmail.com>
2021-04-23 20:41:46 +0300

    Removed the else block to solve the error issue

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,25 +1,93 @@
 import operate from './operate';
 
-const calculate = (data, name) => {
-  let result;
-  const { total, next, operation } = data;
-  switch (name) {
-    case '+/-':
-      result = total * (-1);
-      break;
-    case '.':
-      result = '.';
-      break;
-    case 'AC':
-      result = '';
-      break;
-    case '=':
-      result = operate(total, next, operation);
-      break;
-    default:
+const calculate = (dataObj, buttonName) => {
+  const regex = new RegExp('^[0-9]|[.]+$');
+
+  if (buttonName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+  if (buttonName === '+/-') {
+    if (dataObj.total !== null) {
+      return {
+        total: (Number(dataObj.total) * -1).toString(),
+      };
+    }
+    if (dataObj.next !== null) {
+      return {
+        next: (Number(dataObj.next) * -1).toString(),
+      };
+    }
   }
 
-  return result;
+  if (buttonName !== '=') {
+    if (regex.test(buttonName)) {
+      if (dataObj.total === null) {
+        return {
+          total: buttonName,
+        };
+      } if (dataObj.total !== null && dataObj.operation === null) {
+        return {
+          total: dataObj.total + buttonName,
+        };
+      }
+      if (dataObj.total !== null && dataObj.operation !== null) {
+        if (dataObj.next !== null) {
+          return {
+            next: dataObj.next + buttonName,
+          };
+        }
+        return {
+          next: buttonName,
+        };
+      }
+    } else {
+      if (dataObj.operation === null) {
+        return {
+          operation: buttonName,
+        };
+      }
+      return {
+        total: operate(dataObj.total, dataObj.next, dataObj.operation),
+        next: null,
+        operation: buttonName,
+      };
+    }
+  }
+
+  return {
+    total: operate(dataObj.total, dataObj.next, dataObj.operation) !== 'error' ? operate(dataObj.total, dataObj.next, dataObj.operation) : null,
+    next: null,
+  };
 };
 
 export default calculate;
+
+// import operate from './operate';
+
+// const calculate = (data, name) => {
+//   let result;
+//   const { total, next, operation } = data;
+//   switch (name) {
+//     case '+/-':
+//       result = total * (-1);
+//       break;
+//     case '.':
+//       result = '.';
+//       break;
+//     case 'AC':
+//       result = '';
+//       break;
+//     case '=':
+//       result = operate(total, next, operation);
+//       break;
+//     default:
+//   }
+
+//   return result;
+// };
+
+// export default calculate;
