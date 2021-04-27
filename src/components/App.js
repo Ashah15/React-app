5d2b7430a52150648ac072a36defe1749c34da58
Ashah15 <ashaabdillahi53@gmail.com>
2021-04-28 00:32:11 +0300

    connect the files to app.js using the handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import ButtonPanel from './ButtonPanel';
-import Display from './Display';
-// import calculate from '../logic/calculate';
+// import React from 'react';
+// import ButtonPanel from './ButtonPanel';
+// import Display from './Display';
+// // import calculate from '../logic/calculate';
 
-export default function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+// export default function App() {
+//   return (
+//     <>
+//       <Display />
+//       <ButtonPanel />
+//     </>
+//   );
+// }
+
+import React from 'react';// eslint-disable-line no-unused-vars
+import './App.css';
+import Display from './Display' // eslint-disable-line no-unused-vars
+import ButtonPanel from './ButtonPanel' // eslint-disable-line no-unused-vars
+import calculate from '../logic/calculate' // eslint-disable-line no-unused-vars
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  clickHandler = (button) => {
+    this.setState(calculate(this.state, button))
+  }
+  render() {
+    return (
+      <div className="App" >
+        <Display result={this.state.next || this.state.total || '0'} />
+        <ButtonPanel clickHandler={this.clickHandler} operation={this.state.operation === 'null' ? '' : this.state.operation} />
+      </div>
+    );
+  }
 }
+
+export default App;
