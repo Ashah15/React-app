fd93dc05ac25175d11fdc9c83576c28cab28331a
Ashah15 <ashaabdillahi53@gmail.com>
2021-05-01 00:39:46 +0300

    Fixed errors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import PropTypes from 'prop-types';
-import Big from 'big.js';
+// import PropTypes from 'prop-types';
+// import Big from 'big.js';
 import App from './components/App';
 
 ReactDOM.render(
