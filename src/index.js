b244e69c94a39c90198e5f57c7fd4861241a5268
Ashah15 <ashaabdillahi53@gmail.com>
2021-05-01 00:38:35 +0300

    tie the calculation modules

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-// import PropTypes from 'prop-types';
-// import Big from 'big.js';
-// import App from './components/App';
+import PropTypes from 'prop-types';
+import Big from 'big.js';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
