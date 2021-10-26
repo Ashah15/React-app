64f112fa9cd284a1533aad61cfaf7799ff6129f8
Ashah15 <ashaabdillahi53@gmail.com>
2021-10-26 17:54:37 +0300

    Add tests to the calculate files

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from 'react';
+import App from './components/App';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
